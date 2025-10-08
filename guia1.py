446181a0cc67eb7c65220a8b2779fce1275f8c1f
MansiDiego <mansilladiegomateo@gmail.com>
2025-10-08 20:34:53 -0300

    Completando la guia

--- guia1.py
+++ guia1.py
@@ -1,6 +1,4 @@
 import math #Importamos la libreria para usar algunas funciones
-
-
 #1
 def imprimir_hola_mundo():
     return("hola mundo")
@@ -77,37 +75,195 @@ def es_par(x: int) -> bool:
     return res
 print(es_par(3))
 
-#Ejercicio 2.7 cantidad de pizzas FALTA TERMINAR
-def cantidad_pizzas():
+#Ejercicio 2.7 cantidad de pizzas, dada una cantidad de comensales y una minima cantidad de porciones que se come cada comensal
+#determinar la cantidad de pizzas que necesitamos, me piden que es preferible que sobren porciones y que no falten.
+#por lo tanto uso la funcion math.ceil, que es para que redondee para arriba, por ejemplo 7/2 = 3.5 => devuelve 4 con la funcion
+def cantidad_pizzas(comensales: int, min_cant_porciones:int) -> int:
+    porciones = comensales * min_cant_porciones
+    numeroDePizzas = porciones / 8
+    return (math.ceil(numeroDePizzas)) 
+print(cantidad_pizzas(6,3))
 
-#EJercicio 3 para pedir la longitud nombre de la variable len(nombre)
 
-def es_nombre_largo (nombre: str)->bool:
+
+#3.1
+def alguno_es_0(num1: int, num2:int)->bool:
+    res = num1 == 0 or num2 == 0 
+    return res
+print(alguno_es_0(0,3))
+
+#3.2
+def ambos_son_0(num1: int, num2:int)-> bool:
+    res = num1 == 0 and num2 == 0
+    return res
+print(ambos_son_0(0,0))
+
+#3.3 para pedir la longitud nombre de la variable len(nombre)####
+def es_nombre_largo(nombre: str)->bool:
     return len(nombre) >= 3 and len(nombre) <=8
-
 print(es_nombre_largo("mansilla"))
+
+#3.4 ¿es Bisiesto? (que indica si un año tiene 366 dias)(Un año es bisiesto si es multiplo de 400, o bien de 4 pero no de 100)
+def esBisiesto(año:int)->bool:
+    res = año % 400 == 0 or (año % 4 == 0 and año % 100 != 0) 
+    return res
+print(esBisiesto(2012))
+
+
+#4.1--Definir la función peso de pino(esta laburada sobre centimetros)--
+def peso_de_pino(altura: int)->int:
+    res : int
+    if altura <= 300:
+        res = altura * 3
+    else:
+       res = 300 * 3 + ((altura - 300) * 2)
+    return res
+print(peso_de_pino(500))
+
+#4.1bis--Usando la funcion min y max solamente
+def pesoDelPinoBis(alt: int) -> int:
+    res : int
+    res = min(alt, 300) * 3 + max(alt - 300, 0) * 2 
+    return res
+print(pesoDelPinoBis(300))
+
+#4.2-- definir la funcion "es peso util", es util si pesa mas de 400 y menos de 1000, la funcion recibe un "peso"
+def peso_util(util: int)->bool:
+    return (min(1000, max(400, util))) == util #Observacion, toma los vordes, es decir puede ser 1000 o 400 y funca, sino deberia modificar los bordes, para abajo y para arriba para que sea estricto
+print(peso_util(900))
+
+#estoy haciendo composición de funciones, con max(400, util), si 400> util entonces me va a devolver 400, sino al reves.
+#si util < 1000 entonces me va a devolver "min" o sea util, y lo mismo al reves si util>1000 me devuelve 1000 la funcion min.
+#finalmente lo comparo con util, si es el mismo valor entonces true, si es 400 o 1000 quiere decir que se cayo por los bordes, y por ende no cumple lo pedido
+
+#4.3 sin composición de funciones
+def sirve_pinoSincompo(altura: int) -> bool:
+    peso = peso_de_pino(altura) < 1000 and peso_de_pino(altura)> 400
+    return peso
+print(sirve_pinoSincompo(500))
+
+#4.4 es una compo de las 2, recibe la altura del pino y determina si le sirve a la fabrica
+def sirve_pino(altura: int) -> bool:
+    peso = peso_de_pino(altura)
+    return peso_util(peso)
+print(sirve_pino(200))
+
 
 
 #Ejercicio 5.1
 def devolver_el_doble(numero: int) -> int:
     res : int
-
     if numero % 2 == 0:
         res = 2 * numero
     else:
-        res= numero
-
+        res = numero
     return res
-
-print(devolver_el_doble(4))
-
+print(devolver_el_doble(8))
 #RECORDAR GUARDAR LA VARIABLE PARA LOS CONDICIONALES.
 
+#5.2 
+def devolver_valor_si_es_par_si_no_el_que_sigue(numero:int) -> int:
+    res : int
+    if numero % 2 == 0 :
+        res = numero
+    else:
+        res = numero + 1
+    return res
+print(devolver_valor_si_es_par_si_no_el_que_sigue(18))    
 
+#5.2 BIS "CON DOBLE IF"
+def devolverpar_o_Sig(numero:int) ->int:
+    res : int
+    if numero % 2 == 0:
+        res = numero
+    if numero % 2 != 0:
+        res = numero + 1
+    return  res      
+print(devolverpar_o_Sig(2))
 
-#Ejercicio 6.2 implementar usando while: escribir una funcion que imprima los numeros pares del 10 y el 40.
-def paresdel10al40()->None:
-    i: int = 10
-    while i<=40:
-        print(i)
-        i= i + 2
+#5.2 BIS III CON if elif else, la diferencia es que le puedo asignar como si tuviera un caso base(quiero creer xd)
+def devolverLoMismo(numero: int)-> int:
+    res : int
+    if numero % 2 == 0:
+        res = numero
+    elif numero % 2 !=0:
+        res = numero + 1
+    else:
+        res = 0        
+    return res    
+print(devolverLoMismo(3))  
+#Las 3 formas funcionan, pero con elif puedo meterle mas condiciones
+
+#5.3
+def devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(numero: int) ->int:
+    res : int
+    if numero % 3 == 0 :
+        res = 2 * numero
+    elif numero % 9 == 0 :
+        res = 3 * numero
+    else:
+        res = numero
+    return res
+print(devolver_el_doble_si_es_multiplo3_el_triple_si_es_multiplo9(18))    
+
+def bis2(numero: int) -> int:
+    res: int
+    if numero % 3 == 0 :
+        res = 2 * numero
+    if numero % 9 == 0 :
+        res = 3 * numero
+    else:
+        res = numero
+
+    return res 
+print(bis2(3))  
+
+def bis3(numero : int) ->int:
+    res : int
+    if numero % 3 == 0 :
+        res = 2 * numero
+    elif numero % 9 == 0 :
+        res = 3 * numero  
+    elif numero % 3 != 0 and numero % 9 !=0:
+        res = numero
+    return res
+print(bis3(18))        
+#Cuestionable porque las 3 funciones andan, pero de formas diferentes, la primera si capta la primer guarda con if, la toma como prioridad
+
+#5.4
+def lindo_nombre(nombre:str)->str:
+    res : str
+    if len(nombre) >= 5:
+        res = "Tu nombre tiene muchas letras"
+    else:
+        res = "tu nombre tiene menos de 5 caracteres"
+    return res
+print(lindo_nombre("mate"))
+
+#5.5
+def elRango(numero: int)->str:
+    res : int
+    if numero < 5:
+        res = "Menor a 5"
+    elif numero >= 10 and numero <=20:
+        res = "entre 10 y 20"
+    else:
+        res = "Mayor a 20"
+    return res    
+print(elRango(18))   
+
+#5.6
+def laburar_o_vacaciones(sexo: str, edad: int)-> str:
+    res : str
+    if sexo == "Masculino" and edad > 18 and edad < 65:
+        res = "toca chambear, lo siento maquina!!"
+    elif edad < 18:
+        res= "Andá de vacaciones"
+    elif sexo == "Masculino" and edad > 65:
+        res = "Andá de vacaciones."
+    elif sexo == "Femenino" and edad > 60:
+        res = "Andá de vacaciones"    
+    else:
+        res = "toca chambear, lo siento maquina!!"    
+    return res
+print(laburar_o_vacaciones("Masculino", 66))    
