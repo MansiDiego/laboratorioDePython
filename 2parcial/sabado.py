a5ae58ff920f956ac2f299e00093f99f4854a3af
MansiDiego <mansilladiegomateo@gmail.com>
2025-10-10 12:22:06 -0300

    Restaurando archivos borrados

---
+++ 2parcial/sabado.py
@@ -0,0 +1,16 @@
+#Vamos a tirar un poco de magia para boludear:
+c = "Hola"
+d = 23
+type(c)
+print(type(c))
+print("Hello")
+print("aca")
+
+def cuadrado(x):
+    return  x * x
+
+
+n = int(input("Ingrese un número: "))
+print("El cuadrado es:", cuadrado(n))
+
+#REPL de Python (Read–Eval–Print Loop), o sea la consola interactiva, como ghci en Haskell.
